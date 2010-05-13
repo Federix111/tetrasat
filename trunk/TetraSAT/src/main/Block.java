cc25d980d085efd2816ad33f60c6b599cc9b1957
adnan1210 <adnan1210@afb9385f-ae71-474a-3c17-7ddebf8f5920>
2010-05-13 18:01:50 +0000

    privates + accessors

--- trunk/TetraSAT/src/main/Block.java
+++ trunk/TetraSAT/src/main/Block.java
@@ -2,16 +2,48 @@ package main;
 
 public class Block {
 	
-	public int top;
-	public int right;
-	public int down;
-	public int left;
+	private int top;
+	private int right;
+	private int down;
+	private int left;
 	
 	public Block(int top, int right, int down, int left) {
 		super();
 		this.top = top;
 		this.right = right;
 		this.down = down;
+		this.left = left;
+	}
+
+	public int getTop() {
+		return top;
+	}
+
+	public void setTop(int top) {
+		this.top = top;
+	}
+
+	public int getRight() {
+		return right;
+	}
+
+	public void setRight(int right) {
+		this.right = right;
+	}
+
+	public int getDown() {
+		return down;
+	}
+
+	public void setDown(int down) {
+		this.down = down;
+	}
+
+	public int getLeft() {
+		return left;
+	}
+
+	public void setLeft(int left) {
 		this.left = left;
 	}
 	

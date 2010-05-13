4bf33343d9ebf1a7234bc55aeae455285757aade
adnan1210 <adnan1210@afb9385f-ae71-474a-3c17-7ddebf8f5920>
2010-05-13 17:55:17 +0000

    block class

---
+++ trunk/TetraSAT/src/main/Block.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Block {
+	
+	public int top;
+	public int right;
+	public int down;
+	public int left;
+	
+	public Block(int top, int right, int down, int left) {
+		super();
+		this.top = top;
+		this.right = right;
+		this.down = down;
+		this.left = left;
+	}
+	
+}

4d22fd3749774321de63177773dee225d2122a20
federix111 <federix111@afb9385f-ae71-474a-3c17-7ddebf8f5920>
2010-05-13 20:26:23 +0000

    bella li

---
+++ TetraSAT/src/testCase/SATEncoderTestCase.java
@@ -0,0 +1,49 @@
+package testCase;
+
+import java.util.ArrayList;
+
+import parser.BlockInputParser;
+
+import encoder.SATEncoder;
+import main.Block;
+import main.Field;
+
+public class SATEncoderTestCase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ArrayList<Block> blocks = new ArrayList<Block>();
+		
+//		blocks.add(new Block("0895"));
+//		blocks.add(new Block("7683"));
+//		blocks.add(new Block("5845"));
+//		blocks.add(new Block("2279"));
+//		blocks.add(new Block("9580"));
+//		blocks.add(new Block("8020"));
+//		blocks.add(new Block("7008"));
+//		blocks.add(new Block("1308"));
+//		blocks.add(new Block("2800"));
+//		blocks.add(new Block("9728"));
+//		blocks.add(new Block("3866"));
+//		blocks.add(new Block("4590"));
+//		blocks.add(new Block("9738"));
+//		blocks.add(new Block("9917"));
+//		blocks.add(new Block("0697"));
+//		blocks.add(new Block("0390"));
+		
+		BlockInputParser parser = new BlockInputParser("input/testBlocks");
+		
+		Field field = parser.getField();
+//		for (Block block : blocks) {
+//			field.addBlock(block);
+//		}
+		
+		SATEncoder encoder = new SATEncoder(field);
+		
+		
+	}
+
+}

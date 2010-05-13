e5a58a6ff92806fc59b3ebbc4b9b1c7d44677598
adnan1210 <adnan1210@afb9385f-ae71-474a-3c17-7ddebf8f5920>
2010-05-13 20:02:25 +0000

    input parser and test cases

--- TetraSAT/src/parser/BlockInputParser.java
+++ TetraSAT/src/parser/BlockInputParser.java
@@ -11,32 +11,37 @@ import main.Field;
 
 public class BlockInputParser {
 
-	public Field field;
-	public static final String INPUTFILEPATH="blocks.txt";
+	private Field field;
 	
-	public BlockInputParser(){
+	
+	public BlockInputParser(String fileInputPath){
 		field = new Field();
 		FileReader fr;
 		try {
-			File file = new File(INPUTFILEPATH);
-			if(file.exists()){
-			fr = new FileReader(file);
+//			File file = new File(fileInputPath);
+//			if(file.exists()){
+			fr = new FileReader(fileInputPath);
 			BufferedReader bf = new BufferedReader(fr);
 			String s ="";
 			while((s = bf.readLine()) != null){
 				field.addBlock(new Block(s));
 			}
 			bf.close();
-			System.out.println(field);
-			} else {
-				System.out.println("File not found");
-			}
+//			System.out.println(field);
+//			} else {
+//				System.out.println("File not found");
+//			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
+			System.out.println("File not Found");
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public Field getField() {
+		return field;
 	}
 }
--- TetraSAT/src/testCase/InputParserTestCase.java
+++ TetraSAT/src/testCase/InputParserTestCase.java
@@ -3,13 +3,14 @@ package testCase;
 import parser.BlockInputParser;
 
 public class InputParserTestCase {
-
+	public static final String INPUTFILEPATH="input/blocks.txt";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BlockInputParser inputParser = new BlockInputParser();
+		BlockInputParser inputParser = new BlockInputParser(INPUTFILEPATH);
+		System.out.println(inputParser.getField());
 	}
 
 }
